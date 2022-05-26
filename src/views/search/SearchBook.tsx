53d2bb994c3023bfd44ba048a71fe34430e460ce
Sojborg <sojborg@outlook.com>
2022-05-26 11:30:27 +0200

    Fixed styling on search book components

--- src/views/search/SearchBook.tsx
+++ src/views/search/SearchBook.tsx
@@ -1,20 +1,22 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 import "./SearchBook.scss";
-import { Button } from "@material-ui/core";
-import { SearchViewMode } from "./Search";
-import { IBook } from "../../common/models/IBooksResponse";
+import {Button} from "@material-ui/core";
+import {SearchViewMode} from "./Search";
+import {IBook} from "../../common/models/IBooksResponse";
 
 interface IBookProps {
   book: IBook;
   viewMode: SearchViewMode;
 
   onSaveBook?(book: IBook): void;
+
   onDeleteBook?(bookId: string): void;
+
   onAddBook?(book: IBook): void;
 }
 
 export const SearchBook = (props: IBookProps) => {
-  const { book, viewMode, onAddBook } = props;
+  const {book, viewMode, onAddBook} = props;
   const addButtonRef = useRef<HTMLButtonElement | null>(null);
 
   const onAddToListClick = async () => {
@@ -26,6 +28,7 @@ export const SearchBook = (props: IBookProps) => {
       key={book.id}
       className={`search-book search-book-${SearchViewMode[viewMode]}`}
     >
+      <h4>{book.title}</h4>
       <div
         className={`search-book__content search-book__content-${SearchViewMode[viewMode]}`}
       >
@@ -41,19 +44,19 @@ export const SearchBook = (props: IBookProps) => {
             Add to list
           </Button>
         </div>
-        <img src={book.coverImageUrl} alt={book.coverImageUrl} />
+        <img src={book.coverImageUrl} alt={book.coverImageUrl}/>
         <div
           className={`search-book__description search-book__description-${SearchViewMode[viewMode]}`}
         >
-          <h4>{book.title}</h4>
-          <p className={`search-book__description search-book__synopsis-${SearchViewMode[viewMode]}`}>{book.synopsis}</p>
+          <p
+            className={`search-book__description search-book__synopsis-${SearchViewMode[viewMode]}`}>{book.synopsis}</p>
         </div>
         <div
           className={`search-book__info search-book__info-${SearchViewMode[viewMode]}`}
         >
-          <p>Publisher: {book.publisher}</p>
-          <p>Page count: {book.pageCount}</p>
-          <p>Product id: {book.productId}</p>
+          <p><b>Publisher</b> {book.publisher}</p>
+          <p><b>Page count</b> {book.pageCount}</p>
+          <p><b>Product id</b> {book.productId}</p>
         </div>
       </div>
     </div>
