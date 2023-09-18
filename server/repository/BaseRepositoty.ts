a71e386aceebc21a1f3e4a44408ec5f687984417
jesper-johnsen <jesper.johnsen@schibsted.com>
2023-09-18 08:56:14 +0200

    do we need baserepo?

--- server/repository/BaseRepositoty.ts
+++ server/repository/BaseRepositoty.ts
@@ -13,55 +13,16 @@ export abstract class BaseRepository<T extends IBaseModel> {
   
   }
 
-  async createDocument(document: Model<T>) {    
-    console.log("Creating document: ", document);
-    // await document.save();
+  async createDocument(document: Model<T>) {   
   }
 
   countCollection() {
-    // return new Promise((resolve, reject) => {
-    //   this.client
-    //     .queryDocuments(this.collectionUrl, `SELECT VALUE COUNT(1) FROM ${this.collection}`)
-    //     .toArray((err, results) => {
-    //       if (err) reject(err);
-    //       else {
-    //         for (var queryResult of results) {
-    //           let resultString = JSON.stringify(queryResult);
-    //           console.log(`\tQuery returned ${resultString}`);
-    //         }
-    //         console.log();
-    //         resolve(results);
-    //       }
-    //     });
-    // });
   }
 
   replaceDocument(document: any) {
-    // let documentUrl = `${this.collectionUrl}/docs/${document.id}`;
-    // console.log(`Replacing document:\n${document.id}\n`);
-
-    // return new Promise((resolve, reject) => {
-    //   this.client.replaceDocument(documentUrl, document, (err, result) => {
-    //     if (err) reject(err);
-    //     else {
-    //       resolve(result);
-    //     }
-    //   });
-    // });
   }
 
 
   deleteDocument(id: any) {
-    // let documentUrl = `${this.collectionUrl}/docs/${id}`;
-    // console.log(`Deleting document:\n${id}\n`);
-
-    // return new Promise((resolve, reject) => {
-    //   this.client.deleteDocument(documentUrl, (err, result) => {
-    //     if (err) reject(err);
-    //     else {
-    //       resolve(result);
-    //     }
-    //   });
-    // });
   }
 }
